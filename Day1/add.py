6d3702ca4d9c70e64aa746dfdf0aa57630b25ef5
umayhani <mmuneebkarishma@gmail.com>
2026-02-18 22:53:50 +0530

    add.py

---
+++ Day1/add.py
@@ -0,0 +1,11 @@
+a=int(input("enter the first number:"))
+b=int(input("enter teh second number"))
+c=int(input("enter the third third number:"))
+sum=a+b+c
+print(sum)
+
+
+#-----------problem3------------------------------
+r=int(input("enter the radius of the circle:"))
+area=3.14*r**2
+print(area)10
---
+++ Day1/temp,swap.py
@@ -0,0 +1,13 @@
+a=int(input())
+b=int(input())
+#------before swapping---------------#
+print("before swapping:",a,b)
+a,b=b,a
+#---------------after swapping----------------#
+print("after swapping:",a,b)
+
+
+#-----------pronblem5----------------------#
+celcius=int(input("enter the celcius:"))
+F=(celcius*9/5)+32
+print(F)
