7de73b109578a3c958c91b77770417a396aac663
umayhani <mmuneebkarishma@gmail.com>
2026-02-20 22:44:40 +0530

    Day3

---
+++ Day3/password.py
@@ -0,0 +1,6 @@
+Username=input("Enter the Username:")
+Password=int(input("Eter the Password:"))
+if(Username=="admin" and Password==1234):
+    print("Login successfull")
+else:
+    print("Login Credentias")  
